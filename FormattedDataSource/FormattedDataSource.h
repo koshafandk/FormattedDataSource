193048c60805d58fbffa5996db29d45117afcc17
BRANDERSTUDIO <alexandr.orlov@brander.ua>
2017-11-02 10:44:27 +0200

    Init commit.

---
+++ FormattedDataSource/FormattedDataSource.h
@@ -0,0 +1,19 @@
+//
+//  FormattedDataSource.h
+//  FormattedDataSource
+//
+//  Created by BRANDERSTUDIO on 02.11.2017.
+//  Copyright © 2017 BRANDERSTUDIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormattedDataSource.
+FOUNDATION_EXPORT double FormattedDataSourceVersionNumber;
+
+//! Project version string for FormattedDataSource.
+FOUNDATION_EXPORT const unsigned char FormattedDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormattedDataSource/PublicHeader.h>
+
+
